f1dd7d9c0fcab47af371880666ee15a4de980196
typoverflow <typoverflow@outlook.com>
2024-04-10 00:28:41 +0800

    feat: finish hpl

--- wiserl/algorithm/hpl.py
+++ wiserl/algorithm/hpl.py
@@ -48,23 +48,45 @@ class Decoder(nn.Module):
         out = torch.nn.functional.relu(out)
         return self.unify(out)
 
-
 class HindsightPreferenceLearning(Algorithm):
     def __init__(
         self,
         *args,
-        seq_len: int = 50,
+        expectile: float = 0.7,
+        beta: float = 0.3333,
+        max_exp_clip: float = 100.0,
+        discount: float = 0.99,
+        tau: float = 0.005,
+        seq_len: int = 100,
+        future_len: int = 50,
         z_dim: int = 64,
+        prior_sample: int = 5,
         kl_loss_coef: float = 1.0,
         kl_balance_coef: float = 0.8,
+        vae_steps: int = 100000,
+        reward_steps: int = 100000,
         **kwargs
     ):
+        self.expectile = expectile
+        self.beta = beta
+        self.max_exp_clip = max_exp_clip
+        self.discount = discount
+        self.tau = tau
         self.seq_len = seq_len
+        self.future_len = future_len
         self.z_dim = z_dim
+        self.prior_sample = prior_sample
         self.kl_loss_coef = kl_loss_coef
         self.kl_balance_coef = kl_balance_coef
+        self.vae_steps = vae_steps
+        self.reward_steps = reward_steps
         super().__init__(*args, **kwargs)
-        self.future_attention_mask = torch.tril(torch.ones([seq_len, seq_len]), diagonal=-1).to(self.device)
+        # define the attention mask for future prediction
+        causal_mask = torch.tril(torch.ones([seq_len, seq_len]), diagonal=-1).bool()
+        future_mask = torch.triu(torch.ones([seq_len, seq_len]), diagonal=future_len).bool()
+        self.future_attention_mask = torch.bitwise_or(causal_mask, future_mask).to(self.device)
+        # self.vae_causal_mask = torch.tril(torch.ones([future_len, future_len]), diagonal=-1).bool().to(self.device)
+        self.reward_criterion = torch.nn.BCEWithLogitsLoss(reduction="none")
 
     def setup_network(self, network_kwargs) -> None:
         self.obs_dim = self.observation_space.shape[0]
@@ -76,6 +98,7 @@ class HindsightPreferenceLearning(Algorithm):
             num_layers=enc_kwargs["num_layers"],
             num_heads=enc_kwargs["num_heads"],
             causal=False,
+            seq_len=self.seq_len
         )
         future_proj = GaussianActor(
             input_dim=enc_kwargs["embed_dim"],
@@ -100,18 +123,45 @@ class HindsightPreferenceLearning(Algorithm):
             conditioned_logstd=True,
             hidden_dims=prior_kwargs["hidden_dims"]
         )
+        reward = vars(wiserl.module)[network_kwargs["reward"].pop("class")](
+            input_dim=self.observation_space.shape[0]+self.action_space.shape[0]+self.z_dim,
+            output_dim=1,
+            **network_kwargs["reward"]
+        )
+        actor = vars(wiserl.module)[network_kwargs["actor"].pop("class")](
+            input_dim=self.observation_space.shape[0],
+            output_dim=self.action_space.shape[0],
+            **network_kwargs["actor"]
+        )
+        critic = vars(wiserl.module)[network_kwargs["critic"].pop("class")](
+            input_dim=self.observation_space.shape[0]+self.action_space.shape[0],
+            output_dim=1,
+            **network_kwargs["critic"]
+        )
+        value = vars(wiserl.module)[network_kwargs["value"].pop("class")](
+            input_dim=self.observation_space.shape[0],
+            output_dim=1,
+            **network_kwargs["value"]
+        )
 
         self.network = nn.ModuleDict({
             "future_encoder": future_encoder.to(self.device),
             "future_proj": future_proj.to(self.device),
             "decoder": decoder.to(self.device),
-            "prior": prior.to(self.device)
+            "prior": prior.to(self.device),
+            "reward": reward.to(self.device),
+            "actor": actor.to(self.device),
+            "critic": critic.to(self.device),
+            "value": value.to(self.device)
+        })
+        self.target_network = nn.ModuleDict({
+            "critic": make_target(self.network.critic)
         })
 
     def setup_optimizers(self, optim_kwargs):
         self.optim = {}
         default_kwargs = optim_kwargs.get("default", {})
-        for k in {"future_encoder", "future_proj", "decoder", "prior"}:
+        for k in {"future_encoder", "future_proj", "decoder", "prior", "reward", "actor", "critic", "value"}:
             this_kwargs = default_kwargs.copy()
             this_kwargs.update(optim_kwargs.get(k, {}))
             self.optim[k] = vars(torch.optim)[this_kwargs.pop("class")](
@@ -119,16 +169,124 @@ class HindsightPreferenceLearning(Algorithm):
                 **this_kwargs
             )
 
-    def select_action(self, *args, **kwargs):
-        return None
+    def select_action(self, batch, deterministic: bool=True):
+        obs = batch["obs"]
+        action, *_ = self.network.actor.sample(obs, deterministic=deterministic)
+        return action.squeeze().cpu().numpy()
 
     def train_step(self, batches, step: int, total_steps: int):
-        obs, action, terminal, timestep, mask = itemgetter("obs", "action", "terminal", "timestep", "mask")(batches[0])
+        unlabel_batch, pref_batch = batches
+        if step < self.vae_steps:
+            metrics = self.update_vae(
+                obs=unlabel_batch["obs"],
+                action=unlabel_batch["action"],
+                timestep=unlabel_batch["timestep"],
+                mask=unlabel_batch["mask"]
+            )
+            return metrics
+        else:
+            metrics = self.update_reward(
+                obs_1=pref_batch["obs_1"],
+                obs_2=pref_batch["obs_2"],
+                action_1=pref_batch["action_1"],
+                action_2=pref_batch["action_2"],
+                label=pref_batch["label"]
+            )
+            if step < self.vae_steps+self.reward_steps:
+                return metrics
+            else:
+                agent_metrics = self.update_agent(
+                    obs=unlabel_batch["obs"],
+                    action=unlabel_batch["action"],
+                    next_obs=unlabel_batch["next_obs"],
+                    terminal=unlabel_batch["terminal"]
+                )
+                metrics.update(agent_metrics)
+                return metrics
+
+
+    def update_agent(self, obs, action, next_obs, terminal):
+        with torch.no_grad():
+            self.target_network.eval()
+            q_old = self.target_network.critic(obs, action)
+            q_old = torch.min(q_old, dim=0)[0]
+        # update value
+        v_pred = self.network.value(obs)
+        v_loss = expectile_regression(v_pred, q_old, expectile=self.expectile).mean()
+        self.optim["value"].zero_grad()
+        v_loss.backward()
+        self.optim["value"].step()
+
+        # update actor
+        with torch.no_grad():
+            adv = q_old - v_pred
+            exp_adv = (adv / self.beta).exp().clip(max=self.max_exp_clip)
+        if isinstance(self.network.actor, DeterministicActor):
+            policy_out = torch.sum((self.network.actor.sample(obs)[0] - action)**2, dim=-1, keepdim=True)
+        elif isinstance(self.network.actor, GaussianActor):
+            policy_out = - self.network.actor.evaluate(obs, action)[0]
+        actor_loss = (exp_adv * policy_out).mean()
+        self.optim["actor"].zero_grad()
+        actor_loss.backward()
+        self.optim["actor"].step()
+
+        # update critic
+        with torch.no_grad():
+            repeated_obs_action = torch.concat([obs, action], dim=-1)
+            repeated_obs_action = repeated_obs_action.repeat([5, 1, 1, 1])
+            z_prior, *_ = self.network.prior.sample(repeated_obs_action, deterministic=False)
+            reward = self.network.reward(torch.concat([repeated_obs_action, z_prior], dim=-1)).mean(dim=0)
+            target_q = self.network.value(next_obs)
+            target_q = reward + self.discount * (1-terminal.float())*target_q
+        q_pred = self.network.critic(obs, action)
+        q_loss = (q_pred - target_q.unsqueeze(0)).pow(2).sum(0).mean()
+        self.optim["critic"].zero_grad()
+        q_loss.backward()
+        self.optim["critic"].step()
+
+        return {
+            "loss/q_loss": q_loss.item(),
+            "loss/v_loss": v_loss.item(),
+            "loss/actor_loss": actor_loss.item(),
+            "misc/q_pred": q_pred.mean().item(),
+            "misc/v_pred": v_pred.mean().item(),
+            "misc/adv": adv.mean().item()
+        }
+
+    def update_reward(self, obs_1, obs_2, action_1, action_2, label):
+        obs_action_1 = torch.concat([obs_1, action_1], dim=-1)
+        obs_action_2 = torch.concat([obs_2, action_2], dim=-1)
+        obs_action_total = torch.concat([obs_action_1, obs_action_2], dim=0)
+        with torch.no_grad():
+            z_posterior, _, info = self.network.future_proj.sample(
+                self.network.future_encoder(
+                    inputs=obs_action_total,
+                    timesteps=None, # consistent with vae training
+                    attention_mask=self.future_attention_mask,
+                    do_embedding=True
+                )
+            )
+        reward_total = self.network.reward(torch.concat([obs_action_total, z_posterior], dim=-1))
+        r1, r2 = torch.chunk(reward_total, 2, dim=0)
+        logit = r2.sum(dim=1) - r1.sum(dim=1)
+        label = label.float()
+        reward_loss = self.reward_criterion(logit, label).mean()
+        with torch.no_grad():
+            reward_acc = ((logit > 0) == torch.round(label)).float().mean()
+        self.optim["reward"].zero_grad()
+        reward_loss.backward()
+        self.optim["reward"].step()
+        return {
+            "loss/reward_loss": reward_loss.item(),
+            "loss/reward_acc": reward_acc.item()
+        }
+
+    def update_vae(self, obs: torch.Tensor, action: torch.Tensor, timestep: torch.Tensor, mask: torch.Tensor):
         B, L, *_ = obs.shape
         obs_action = torch.concat([obs, action], dim=-1)
         out = self.network.future_encoder(
             inputs=obs_action,
-            timesteps=timestep,
+            timesteps=None, # here we don't use the timestep from dataset, but use the default `np.arange(len)`
             attention_mask=self.future_attention_mask,
             key_padding_mask=(1-mask).squeeze(-1).bool(),
             do_embedding=True
@@ -150,7 +308,6 @@ class HindsightPreferenceLearning(Algorithm):
         input_obs_action = obs_action[:, x, :]
         input_z_posterior = z_posterior[:, x, :]
         input_delta_t = (y - x).repeat(B, 1)
-        # input_loss_mask = mask[:, y, :]
         target_obs_action = obs_action[:, y, :]
         pred_obs_action = self.network.decoder(
             input_obs_action,
@@ -159,7 +316,6 @@ class HindsightPreferenceLearning(Algorithm):
         )
 
         recon_loss = torch.nn.functional.mse_loss(pred_obs_action, target_obs_action, reduction="none")
-        # recon_loss = (recon_loss * input_loss_mask).mean(-1).sum() / input_loss_mask.sum()
         recon_loss = recon_loss.mean()
 
         # KL divergence
@@ -167,10 +323,6 @@ class HindsightPreferenceLearning(Algorithm):
         prior_kl_loss = compute_kl_loss(z_mean.detach(), z_logstd.detach(), z_prior_mean, z_prior_logstd).mean()
         post_kl_loss = compute_kl_loss(z_mean, z_logstd, z_prior_mean.detach(), z_prior_logstd.detach()).mean()
         kl_loss = self.kl_balance_coef * prior_kl_loss + (1-self.kl_balance_coef) * post_kl_loss
-        # z_var = (2*z_logstd).exp()
-        # z_piror_var = (2*z_prior_logstd).exp()
-        # kl_loss = z_prior_logstd - z_logstd + (z_var + (z_mean - z_prior_mean).square()) / (2*z_piror_var) - 0.5
-        # kl_loss = kl_loss.mean()
 
         self.optim["future_encoder"].zero_grad()
         self.optim["future_proj"].zero_grad()
